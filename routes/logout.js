6a2a516e44d5120b6f5506583e64c9bfb3bcbc34
a-idris <a.idris.1@warwick.ac.uk>
2018-03-10 18:38:05 +0000

    cookie expiry fix

--- routes/logout.js
+++ routes/logout.js
@@ -4,6 +4,10 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
     // destroy the session
     req.session = null;
+    // clear cookies
+    res.clearCookie('xpub_key')
+    res.clearCookie('change_index')
+    res.clearCookie('external_index')
     // redirect home
     res.redirect('/');
 });
